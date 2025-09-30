48c9fa4d2f160fe2a15912663409e99d30ee479d
nishat444 <nishat444@gmail.com>
2025-09-30 22:16:44 +0100

    Initial commit: STEM Learn website

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve all static files from /public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Default route → send index.html inside /public
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Listen on all network interfaces
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
+
